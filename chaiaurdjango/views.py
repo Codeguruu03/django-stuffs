c428750f0345da365d4ae30ba681905333cf6582
Codeguruu <namanmaheshgoyal@gmail.com>
2024-07-11 17:35:13 +0530

    first app created in django

---
+++ chaiaurdjango/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse('Hello, World!. you are at chai aur Django home page')
+
+def about(request):
+    return HttpResponse('Hello, World!. you are at chai aur Django about page')
+
+def contact(request):
+    return HttpResponse('Hello, World!. you are at chai aur Django contact page')
