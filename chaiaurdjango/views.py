302d429d8bc39c7f1b4816cb2f5fce1577709014
Codeguruu <namanmaheshgoyal@gmail.com>
2024-07-13 20:32:00 +0530

    first index file is added

--- chaiaurdjango/views.py
+++ chaiaurdjango/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse('Hello, World!. you are at chai aur Django home page')
+    # return HttpResponse('Hello, World!. you are at chai aur Django home page')
+    return render(request, 'index.html')
 
 def about(request):
     return HttpResponse('Hello, World!. you are at chai aur Django about page')
